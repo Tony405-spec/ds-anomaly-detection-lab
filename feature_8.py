7840c5c71817bd68c78b6b8949ba239ae2485d6e
Tony405-spec <tony405-spec@users.noreply.github.com>
2026-04-30 09:59:18 +0300

    Add feature 8
    Co-authored-by: Kate020-cpu <kate020-cpu@users.noreply.github.com>
    Co-authored-by: monicloraine94 <monicloraine94@users.noreply.github.com>

---
+++ feature_8.py
@@ -0,0 +1,11 @@
+﻿def feature_8(data):
+    '''
+    Process data for feature 8
+    
+    Args:
+        data: Input list of numbers
+        
+    Returns:
+        Processed list
+    '''
+    return [x * 8 for x in data]
