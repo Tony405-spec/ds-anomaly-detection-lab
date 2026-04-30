bb55821a2c6d5b834f0bdac8ca361b728094f7b5
Tony405-spec <tony405-spec@users.noreply.github.com>
2026-04-30 09:56:17 +0300

    Add feature 2
    Co-authored-by: Kate020-cpu <kate020-cpu@users.noreply.github.com>
    Co-authored-by: monicloraine94 <monicloraine94@users.noreply.github.com>

---
+++ feature_2.py
@@ -0,0 +1,11 @@
+﻿def feature_2(data):
+    '''
+    Process data for feature 2
+    
+    Args:
+        data: Input list of numbers
+        
+    Returns:
+        Processed list
+    '''
+    return [x * 2 for x in data]
